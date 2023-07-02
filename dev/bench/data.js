91f33a51a6b7c194944ea448cca50c74d5abe300
github-action-benchmark <github@users.noreply.github.com>
2023-07-02 12:46:33 +0000

    add Benchmark (cargo) benchmark result for 670ac0c1d4c1bf874aaae4f710f435b08ecc81ce

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1688301993623,
+  "repoUrl": "https://github.com/jortrr/mandelbrot",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "JRR.vanWaes@student.han.nl",
+            "name": "Jort vanWaes",
+            "username": "jortrr"
+          },
+          "committer": {
+            "email": "JRR.vanWaes@student.han.nl",
+            "name": "Jort vanWaes",
+            "username": "jortrr"
+          },
+          "distinct": true,
+          "id": "670ac0c1d4c1bf874aaae4f710f435b08ecc81ce",
+          "message": "Added a benchmark for MandelbrotSet::iterate in benches/bench.rs (uses cargo bench). Added the Continuous Benchmark action to rust.yml",
+          "timestamp": "2023-07-02T14:39:21+02:00",
+          "tree_id": "4690fd8257064bea80c3bbd8bbf57894604f1404",
+          "url": "https://github.com/jortrr/mandelbrot/commit/670ac0c1d4c1bf874aaae4f710f435b08ecc81ce"
+        },
+        "date": 1688301993140,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_mandelbrot_set_iterate",
+            "value": 78831,
+            "range": "± 256",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
